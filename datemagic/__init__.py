7c13bd0f4bb5d6b68672705d7a9e71f1109fab7b
Rustam Gilyazov <gilyazov@gmail.com>
2017-02-07 21:19:30 +1300

    Added docs and rebuilt package

--- datemagic/__init__.py
+++ datemagic/__init__.py
@@ -1,1 +1,15 @@
-from .datemagic import *
+# -*- coding: utf-8 -*- 
+"""
+=========
+Datemagic
+=========
+Convenience functions for operating with dates and date intervals.
+"""
+__title__ = 'datemagic'
+__version__ = '0.1'
+__build__ = 0x001100
+__author__ = 'Rustam Gilyazov'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2017 Rustam Gilyazov'
+
+from .datemagic import split_interval, split_interval_int
